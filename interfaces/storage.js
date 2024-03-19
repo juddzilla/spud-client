3c6bf404b7256e25f0ae73c3d2c4bf3bafa27b92
Judd Hendrix <juddhendrix@gmail.com>
2024-03-18 20:13:48 -0700

    storage class

--- interfaces/storage.js
+++ interfaces/storage.js
@@ -1,1 +1,20 @@
 // npm install @react-native-async-storage/async-storage
+import * as SecureStore from 'expo-secure-store';
+
+class Storage {
+    async get(key) {
+        const value = await SecureStore.getItemAsync(key);
+        return JSON.parse(value);
+    }
+
+    async remove(key) {
+        await SecureStore.deleteItemAsync(key, value);
+        return true;
+    }
+
+    async set(key, value) {
+        await SecureStore.setItemAsync(key, JSON.stringify(value));
+    }
+}
+
+export default new Storage();
